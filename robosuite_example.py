b107fcf72b5de1f05ec00e96d158454a8d398b1d
Brian <b.lee@utexas.edu>
2023-07-09 22:45:06 +0900

    added some basic robosuite example

---
+++ robosuite_example.py
@@ -0,0 +1,69 @@
+# %%
+import robosuite as suite
+from robosuite.wrappers.gym_wrapper import GymWrapper
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %% To Do List
+# TODO: Figure out how to access robot's camera observations - seems to be included in obs variable
+# TODO: Figure out what the observation array represents
+
+# %% Camera observation
+env = suite.make(
+    env_name                = 'Lift',
+    robots                  = 'Panda',
+    has_renderer            = True,
+    has_offscreen_renderer  = True,
+    use_camera_obs          = True,
+    render_camera           = 'agentview', # ['frontview', 'agentview', None, ?]
+    reward_shaping          = True,
+    camera_names            = ['frontview'], # ['frontview', 'agentview', None, ?]
+)
+
+# %% No camera observation
+# env = suite.make(
+#     env_name                = 'Lift',
+#     robots                  = 'Panda',
+#     has_renderer            = True,
+#     has_offscreen_renderer  = False,
+#     use_camera_obs          = False,
+#     reward_shaping          = True,
+# )
+
+# 42 for use_camera_obs = False
+# 393258 for ['frontview', 'agentview']
+# 196650 for ['frontview'] or ['agentview']
+
+# 393258 - 196650 = 196650 - 42 = 196608
+# Each image frame has 196608 frames. Just not sure how it's organized
+
+# 196608 / 3 = 65536
+# Total of 65536 pixels, assuming RGB channel
+
+# 256 * 256 = 65536
+
+# %%
+env = GymWrapper(env)
+
+# %%
+obs = env.reset()
+done = False
+
+while not done:
+    action = env.action_space.sample()  # Replace with your own action selection logic
+    obs, reward, done, info = env.step(action)
+
+    # import pdb; pdb.set_trace()
+    print(obs.shape)
+    print('obs: ', obs)
+    print('reward: ', reward)
+
+    # Render camera observation
+    img = obs[-256*256*3:] + 1
+    img = np.reshape(img, (256, 256, 3)).astype(np.uint8)
+    plt.imshow(img)
+    plt.savefig('img_agentcamera.png')
+    import pdb; pdb.set_trace()
+
+    env.render()  # Remove this line if has_renderer=False
