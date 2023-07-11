b211a975d24096765befa01ff57fe017a32a59ed
Brian <b.lee@utexas.edu>
2023-07-12 03:41:27 +0900

    added working example of gym + sb3 with visualizations

---
+++ experiments/gym_sb3_example.py
@@ -0,0 +1,28 @@
+# %%
+import os
+import gym
+
+from stable_baselines3.sac.policies import MlpPolicy
+from stable_baselines3 import SAC
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+# %%
+env = gym.make('Pendulum-v1', render_mode='human')
+
+model = SAC(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=50000, log_interval=10)
+model.save('sac_pendulum')
+
+del model # remove to demonstrate saving and loading
+
+# %%
+model = SAC.load('sac_pendulum')
+
+obs = env.reset()[0]
+
+while True:
+    action = model.predict(obs)
+    obs, rewards, dones, info, _ = env.step(action[0])
+    print(rewards)
+    env.render()
