f48c9ccccbed4abdebfc9d0f8544e855098b1f0f
Brian <b.lee@utexas.edu>
2023-07-13 07:50:27 +0900

    some minor code improvements (print statements, etc)

--- experiments/robosuite_sb3.py
+++ experiments/robosuite_sb3.py
@@ -1,10 +1,9 @@
 # %%
 import os
-from datetime import datetime
 import robosuite as suite
 from robosuite.wrappers.gym_wrapper import GymWrapper
 from stable_baselines3 import SAC
-from stable_baselines3.sac.policies import MlpPolicy
+from stable_baselines3.sac.policies import MlpPolicy, CnnPolicy # TODO Use CNN policy for camera obs
 
 # %% Camera observation
 env = suite.make(
@@ -26,12 +25,12 @@ done = False
 tb_logger_name = datetime.now().strftime('SAC_%Y-%m-%d_%H-%M')
 
 model = SAC(policy='MlpPolicy', env=env, verbose=1)
-model.learn(total_timesteps=100000, log_interval=1, tb_log_name=tb_logger_name)
+model.learn(total_timesteps=3000000, log_interval=1, tb_log_name=tb_logger_name)
 model.save('PandaLift'); print('Model saved.')
 del model
 
 # %%
-model = SAC.load('PandaLift', env=env)
+model = SAC.load('20230712_PandaLift_3000000', env=env)
 episodes = 10
 rewards = []
 
@@ -40,13 +39,29 @@ for ep in range(episodes):
     episode_reward, timestep, done = 0, 0, False
 
     while not done:
-        print('Timestep: ', timestep); timestep += 1
         action = model.predict(obs)
         obs, reward, dones, info = env.step(action[0])
         episode_reward += reward
         env.render()
-        # print('reward:', reward)
+        print('Timestep: {}\t Reward: {:.5f}\t CumReward: {:.3f}'.format(timestep, reward, episode_reward))
+        timestep += 1
     rewards.append(episode_reward)
-    print('='*90, '\n' 'Episode: {}, Reward: {}'.format(ep, episode_reward), '\n', '='*90)
+    print('='*90)
+    print('Episode: {}, Reward: {:.5f}'.format(ep, episode_reward))
+    print('='*90)
 
 env.close()
+
+# %%
+# Last run started 20230712 10:28 AM
+# Run completed 14:53 20230712 (4.5 hours)
+# total_timesteps = 1000000
+
+# Last run started 20230712 18:00
+# Run completed  20230713 07:43 (13.75 hours)
+# total_timesteps = 3000000
+
+# %%
+# Try using the camera inputs from the model's head camera
+# Replace the observation space with the camera observation space
+# Replace MlpPolicy with CnnPolicy
