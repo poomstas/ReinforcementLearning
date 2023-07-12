78991b6badc2d969db9ffd9173031fb19fcdbf3f
Brian <b.lee@utexas.edu>
2023-07-12 09:07:58 +0900

    cleaned up code

--- experiments/gym_sb3_example.py
+++ experiments/gym_sb3_example.py
@@ -1,15 +1,15 @@
 # %%
 import os
 import gym
-
 from stable_baselines3.sac.policies import MlpPolicy
 from stable_baselines3 import SAC
 
+# %%
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
-# %%
 env = gym.make('Pendulum-v1', render_mode='human')
 
+# %%
 model = SAC(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=50000, log_interval=10)
 model.save('sac_pendulum')
